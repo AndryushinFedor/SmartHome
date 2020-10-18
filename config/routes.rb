1b1580e9652c80c966219e44918dc4a19e6ebd3c
Fedor <andryushin.fedor@gmail.com>
2020-10-18 15:30:32 +0300

    added main pages + posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :subscribers
+  get 'posts/index'
+  get 'promo', to: 'promo#index'
+  get 'about', to: 'about#index'
+
+  root 'promo#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+post = Post.create(
+	name: "New Album",
+	title: "Bad Seeds",
+	author: "Nick Cave",
+	content: "Tracklist"
+)
+
+puts "Some magic just create a #{ post.name } with id #{ post.id }!"
+
+posts = [
+{
+	name: "New Album",
+	title: "The Bad Seeds",
+	author: "Nick Cave",
+	content: "Tracklist"
+},
+{
+	name: "Old Album",
+	title: "The Bad Seeds Old",
+	author: "Nick Cave Old Too",
+	content: "Some Old Tracklist"
+}
+]
+
+posts.each do |post|
+	postik = Post.create(post)
+	puts "Some magic just create a #{ postik.name } with id #{ postik.id }!"
+end
