c3aa71bbc6f1f6d093358c4936c80707978e540b
Fedor <andryushin.fedor@gmail.com>
2021-06-24 23:29:07 +0300

    heroku upd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: 'https://paas:0918d0baedc123a91c53292b177feeed@oin-us-east-1.searchly.com'
 end
